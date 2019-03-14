95fe0ddcecf8d3ae40ac82353c098e8f72438a67
afiebig <alfredo.fiebig@continuum.cl>
2019-03-14 11:02:40 -0300

    Use the new POS object from SDK

--- TransbankPosSDKExample/Form1.cs
+++ TransbankPosSDKExample/Form1.cs
@@ -7,22 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Transbank.pos;
-
+using Transbank.POS;
+using Transbank.POS.Wrapper;
 namespace TransbankPosSDKExample
 {
     public partial class Form1 : Form
     {
         private string portName = "";
         private int total = 0;
+        private POS pos;
 
         public Form1()
         {
+            CenterToScreen();
             InitializeComponent();
             portName_lbl.Text = portName;
             port_ddown.SelectedIndex = 0;
             portName = port_ddown.SelectedItem.ToString();
             total_price_lbl.Text = total.ToString();
+            pos = new POS();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -47,7 +50,7 @@ namespace TransbankPosSDKExample
 
         private void pollingToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (transbank.polling() == transbank.TBK_OK)
+            if (pos.Polling())
             {
                 MessageBox.Show("POS is connected.", "Polling the POS");
             }
@@ -59,15 +62,13 @@ namespace TransbankPosSDKExample
 
         private void connect_btn_Click(object sender, EventArgs e)
         {
-            transbank.select_port(portName);
-            transbank.configure_port();
-            transbank.open_configured_port();
-            portName_lbl.Text = portName = transbank.get_configured_port_name();
+            pos.OpenPort(portName, tbk_baudrate.TBK_115200);
+            portName_lbl.Text = portName = TransbankWrap.get_configured_port_name();
         }
 
         private void disconnect_btn_Click(object sender, EventArgs e)
         {
-            transbank.close_port();
+            TransbankWrap.close_port();
             portName_lbl.Text = portName = "";
         }
     }
