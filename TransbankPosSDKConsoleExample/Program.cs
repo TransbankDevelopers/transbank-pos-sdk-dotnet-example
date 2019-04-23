823a58bfa1a2b0103019fbcd2ddab376fa1dfc59
afiebig <alfredo.fiebig@continuum.cl>
2019-04-23 11:46:32 -0400

    Update ConsoleExample to add Sale Transaction

--- TransbankPosSDKConsoleExample/Program.cs
+++ TransbankPosSDKConsoleExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Transbank.POS;
 using Transbank.POS.Utils;
 
@@ -8,12 +9,29 @@ namespace TransbankPosSDKConsoleExample
     {
         static void Main(string[] args)
         {
-            string portName = "COM4";
+            List<string> ports = Serial.ListPorts();
+            string portName;
 
-            POS pos = new POS();
+            int i = 0;
+            Console.WriteLine("Select serial port to use:");
+            foreach(string port in ports)
+            {
+                Console.WriteLine(i + ": " + port);
+                i++;
+            }
 
-            pos.OpenPort(portName, TbkBaudrate.TBK_115200);
-            Console.WriteLine("Serial Port succesfuly opend");
+            ConsoleKeyInfo menu = Console.ReadKey();
+            int selected = (int)char.GetNumericValue(menu.KeyChar);
+            if (selected >= 0 && selected <= i) {
+                portName = ports[selected];
+            }
+            else
+            {
+                return;
+            }
+
+            POS.Instance.OpenPort(portName, TbkBaudrate.TBK_115200);
+            Console.WriteLine("\nSerial Port succesfuly opend");
             Console.WriteLine("Showing Menu");
             ConsoleKeyInfo key = ShowMenu();
             Console.WriteLine("\n");
@@ -22,11 +40,49 @@ namespace TransbankPosSDKConsoleExample
                 switch (key.KeyChar)
                 {
                     case '1':
-                        if (pos.Polling())
+                        if (POS.Instance.Polling())
                             Console.WriteLine("Pos Connected");
                         else
                             Console.WriteLine("Pos NOT Connected");
                             break;
+
+                    case '2':
+                        try
+                        {
+                            var response = POS.Instance.LoadKeys();
+                            Console.WriteLine(response);
+                            break;
+                        }catch(Exception e)
+                        {
+                            throw;
+                        }
+
+                    case '3':
+                        try
+                        {
+                            var response = POS.Instance.RegisterClose();
+                            Console.WriteLine(response);
+                            break;
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Error: " + e.Message);
+                            break;
+                        }
+
+                    case '4':
+                        try
+                        {
+                            var response = POS.Instance.Sale(2500, 1);
+                            Console.WriteLine(response);
+                            break;
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Error: " + e.Message);
+                            break;
+                        }
+
                     default:
                         Console.WriteLine("Invalid Option");
                         break;
@@ -41,6 +97,9 @@ namespace TransbankPosSDKConsoleExample
             Console.WriteLine(
                 "Seleccione un commando:\n" +
                 "\t(1)\t Polling\n" +
+                "\t(2)\t Load Keys\n" +
+                "\t(3)\t Register Close\n" +
+                "\t(4)\t Sample Sale\n" +
                 "\t(0)\t Exit\n"
                 );
             return Console.ReadKey();
