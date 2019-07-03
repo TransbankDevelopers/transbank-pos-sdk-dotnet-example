77f92eeb6603c48bfbf7525c78287f8267a1e66b
afiebig <alfredo.fiebig@continuum.cl>
2019-07-03 16:11:38 -0400

    Change button order and Change Text

--- TransbankPosSDKExample/DetailPrompt.Designer.cs
+++ TransbankPosSDKExample/DetailPrompt.Designer.cs
@@ -36,17 +36,19 @@
             // infoDetailPrompt
             // 
             this.infoDetailPrompt.AutoSize = true;
-            this.infoDetailPrompt.Location = new System.Drawing.Point(87, 31);
+            this.infoDetailPrompt.Location = new System.Drawing.Point(174, 60);
+            this.infoDetailPrompt.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.infoDetailPrompt.Name = "infoDetailPrompt";
-            this.infoDetailPrompt.Size = new System.Drawing.Size(162, 13);
+            this.infoDetailPrompt.Size = new System.Drawing.Size(328, 25);
             this.infoDetailPrompt.TabIndex = 0;
             this.infoDetailPrompt.Text = "Obtener listado de transacciones";
             // 
             // printBtn
             // 
-            this.printBtn.Location = new System.Drawing.Point(108, 70);
+            this.printBtn.Location = new System.Drawing.Point(216, 214);
+            this.printBtn.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.printBtn.Name = "printBtn";
-            this.printBtn.Size = new System.Drawing.Size(120, 23);
+            this.printBtn.Size = new System.Drawing.Size(240, 44);
             this.printBtn.TabIndex = 1;
             this.printBtn.Text = "Imprimir en POS";
             this.printBtn.UseVisualStyleBackColor = true;
@@ -54,22 +56,24 @@
             // 
             // noPrintBtn
             // 
-            this.noPrintBtn.Location = new System.Drawing.Point(108, 109);
+            this.noPrintBtn.Location = new System.Drawing.Point(216, 132);
+            this.noPrintBtn.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.noPrintBtn.Name = "noPrintBtn";
-            this.noPrintBtn.Size = new System.Drawing.Size(120, 23);
+            this.noPrintBtn.Size = new System.Drawing.Size(240, 44);
             this.noPrintBtn.TabIndex = 2;
-            this.noPrintBtn.Text = "No imprimir";
+            this.noPrintBtn.Text = "Por Pantalla";
             this.noPrintBtn.UseVisualStyleBackColor = true;
             this.noPrintBtn.Click += new System.EventHandler(this.noPrintBtn_Click);
             // 
             // DetailPrompt
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(347, 188);
+            this.ClientSize = new System.Drawing.Size(694, 362);
             this.Controls.Add(this.noPrintBtn);
             this.Controls.Add(this.printBtn);
             this.Controls.Add(this.infoDetailPrompt);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "DetailPrompt";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Detalle de Ventas";
